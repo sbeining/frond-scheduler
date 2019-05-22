43358c274253da1032dfe552ba6ad0b4f79616f6
Sascha Beining <beining@saltation.de>
2019-05-22 21:18:16 +0200

    Add login form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,12 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table("`user`")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -28,6 +29,7 @@ class User
     private $roles = [];
 
     /**
+     * @var string The hashed password
      * @ORM\Column(type="string")
      */
     private $password;
@@ -37,6 +39,11 @@ class User
         return $this->id;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
     public function getUsername(): string
     {
         return (string) $this->username;
@@ -49,9 +56,16 @@ class User
         return $this;
     }
 
+    /**
+     * @see UserInterface
+     */
     public function getRoles(): array
     {
-        return $this->roles;
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
     }
 
     public function setRoles(array $roles): self
@@ -61,6 +75,9 @@ class User
         return $this;
     }
 
+    /**
+     * @see UserInterface
+     */
     public function getPassword(): string
     {
         return (string) $this->password;
@@ -71,5 +88,22 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
