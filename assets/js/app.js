bac6b2bec1e09eb8756babd98ae59b030dd43e3a
Sascha Beining <beining@saltation.de>
2019-05-24 00:41:23 +0200

    Add a basic bootstrap theme to the calendar
    The bootstrap4 fullcalendar theme need fontawesome.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/global.scss');
 require('../css/app.css');
+require('@fortawesome/fontawesome-free/css/all.min.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
@@ -20,14 +21,17 @@ console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/bootstrap/main.css';
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import bootstrap from '@fullcalendar/bootstrap';
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
 
   var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
+    plugins: [ dayGridPlugin, bootstrap ],
+    themeSystem: 'bootstrap'
   });
 
   calendar.render();
