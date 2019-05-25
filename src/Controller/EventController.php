4b2894e7063b6d742d05030870be69ef7546e77f
Sascha Beining <beining@saltation.de>
2019-05-25 22:02:56 +0200

    Restrict access to foreign events

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -99,6 +99,10 @@ class EventController extends AbstractController
      */
     public function edit(Request $request, Event $event): Response
     {
+        if ($event->getUser() !== $this->getUser()) {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
+
         $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
@@ -121,6 +125,10 @@ class EventController extends AbstractController
      */
     public function delete(Request $request, Event $event): Response
     {
+        if ($event->getUser() !== $this->getUser()) {
+            $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        }
+
         if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($event);
