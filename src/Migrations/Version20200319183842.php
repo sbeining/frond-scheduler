3d16bf30af9c4ba6ed79567217bc98b58646186a
Sascha Beining <sbeining@Saschas-MacBook-Air.local>
2020-03-19 19:44:14 +0100

    Update to Symfony 4.3

---
+++ src/Migrations/Version20200319183842.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200319183842 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE event ALTER id DROP DEFAULT');
+        $this->addSql('ALTER TABLE "user" ALTER id DROP DEFAULT');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON "user" (username)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_8D93D649F85E0677');
+        $this->addSql('CREATE SEQUENCE user_id_seq');
+        $this->addSql('SELECT setval(\'user_id_seq\', (SELECT MAX(id) FROM "user"))');
+        $this->addSql('ALTER TABLE "user" ALTER id SET DEFAULT nextval(\'user_id_seq\')');
+        $this->addSql('CREATE SEQUENCE event_id_seq');
+        $this->addSql('SELECT setval(\'event_id_seq\', (SELECT MAX(id) FROM event))');
+        $this->addSql('ALTER TABLE event ALTER id SET DEFAULT nextval(\'event_id_seq\')');
+    }
+}
